6700bdb5c5bb278007ecc21e2acbe347879f282b
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2018-03-01 20:49:15 +0100

    cars working

--- Car.py
+++ Car.py
@@ -9,35 +9,47 @@ class Car:
         self.available = True #indice de la course en cours
         self.rides = []
         self.elapsed_time = 0
+        self.next_ride = None
         self.moving_towards_ride = False
 
     def move(self):
         if self.moving_towards_ride:
-            ride = self.rides[-1]
-            ydest = ride.end_inter_col
-            xdest = ride.end_inter_row
+            ydest = self.next_ride.end_inter_col
+            xdest = self.next_ride.end_inter_row
         else:
             xdest = self.xdest
             ydest = self.ydest
         
         if self.xpos != xdest:
-            self.xpos += 1 
+            if self.xpos > xdest:
+                self.xpos -= 1 
+            else:
+                self.xpos += 1
         elif self.ypos != ydest:
-            self.ypos += 1
+            if self.ypos > ydest:
+                self.ypos -= 1
+            else:
+                self.ypos += 1
         else:
             if not self.moving_towards_ride:
                 self.available = True
             else:
                 self.moving_towards_ride = False
+                self.rides.append(self.next_ride)
+                self.xdest = self.next_ride.end_inter_row
+                self.ydest = self.next_ride.end_inter_col
+                self.next_ride = None
 
     def add_ride(self, ride):
         if self.xpos == ride.start_inter_row and self.ypos == ride.start_inter_col:
             self.moving_towards_ride = False
+            self.rides.append(ride)
+            self.xdest = ride.end_inter_row
+            self.ydest = ride.end_inter_col
         else:
             self.moving_towards_ride = True
+            self.next_ride = ride
         self.available = False
-        self.rides.append(ride)
-
 
     def compute_time(self):
         if self.available:
@@ -54,3 +66,13 @@ class Car:
                                                                 self.ypos,
                                                                 self.xdest,
                                                                 self.ydest)
+
+
+    def time_distance(self, ride):
+        expected_time = 0
+        if not self.available:
+            expected_time += abs(self.xdest - self.xpos)
+            expected_time += abs(self.ydest - self.ypos)
+        expected_time += abs(self.xdest - ride.start_inter_row)
+        expected_time += abs(self.ydest - ride.start_inter_col)
+        return expected_time
--- main.py
+++ main.py
@@ -14,20 +14,21 @@ for i in range(0,F):
 
 rides = sort_rides(rides)
 moving_cars = []
-temps = 0
 
-while True:
+time = 0
+while time <= T:
     for earliest_start, _rides in rides.items():
         for ride in _rides:
             x = ride.start_inter_row
             y = ride.start_inter_col
             t0 = earliest_start
             av_cars = find_closets_available_cars_by_dest(cars, x, y, t0)
-            if av_cars:
-                car = av_cars[0]
-                car.add_ride(ride)
-            else:
-                # TODO: check cars...
-                
-
-    temps += 1
+            for car in av_cars:
+                if car.time_distance(ride) <= earliest_start + ride.distance():
+                    car.add_ride(ride)
+            rides[earliest_start].remove(ride)     
+    
+    for key, car in cars.items():
+        car.move()
+        print(car, car.rides)
+    time += 1
